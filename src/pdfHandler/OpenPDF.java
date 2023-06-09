c2a33085fcbefbfc46535b80539cbb853640371f
Jeff Baker <git@jbaker.net>
2023-06-09 11:58:01 -0600

    Begin using PDFBox

---
+++ src/pdfHandler/OpenPDF.java
@@ -0,0 +1,14 @@
+package pdfHandler;
+import org.apache.pdfbox.pdmodel.PDDocument;
+
+public class OpenPDF
+{
+
+  
+  public static void main(String[] args)
+  {
+    String testy = "A";
+//    System.out.println(testy2);
+
+  }
+}
