efa713d5fdec8a2977a8bca8ea4e9e19bcb4a7d4
Jeff Baker <git@jbaker.net>
2023-06-16 07:26:45 -0600

    Adjustments to open screen layout

--- src/userInterface/Launcher.java
+++ src/userInterface/Launcher.java
@@ -1,5 +1,9 @@
 package userInterface;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.FlowLayout;
 import java.io.File;
 import javax.swing.*;
 
@@ -19,13 +23,29 @@ public class Launcher
   {
     JFrame openScreen = new JFrame(PROGRAM_NAME);
     
+    openScreen.setSize(520, 350);
+    openScreen.setLayout(new BorderLayout(10, 5));
+    openScreen.setLocationRelativeTo(null);
+    openScreen.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    openScreen.setResizable(false);
+    openScreen.setVisible(true);
+    
+    JPanel panel = new JPanel();
+//    FlowLayout fLayout = new FlowLayout(FlowLayout.CENTER, 10, 10);
+    BoxLayout layout = new BoxLayout(panel, BoxLayout.Y_AXIS);
+    panel.setLayout(layout);
+    panel.setBackground(Color.WHITE);
+    openScreen.add(panel);
+    
     String openingText = "<html><p>Welcome to " + PROGRAM_NAME + "! " +
       "To begin, please select a PDF file of your manuscript.</p></html>";
     JLabel openLabel = new JLabel(openingText);
-    openLabel.setBounds(50, 50, 370, 50);
+    openLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+//    openLabel.setBounds(50, 50, 370, 50);
     
     JButton selectButton = new JButton("Open Manuscript PDF");
-    selectButton.setBounds(100, 150, 300, 50);
+    selectButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+//    selectButton.setBounds(100, 150, 300, 50);
     selectButton.addActionListener(e -> {
 //      openFileSelector(chooseFile);
 //      fileSelector = new FileSelector();
@@ -40,18 +60,15 @@ public class Launcher
     });
     
     JButton closeButton = new JButton("Close");
-    closeButton.setBounds(100, 220, 300, 50);
+    closeButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+//    closeButton.setBounds(100, 220, 300, 50);
     closeButton.addActionListener(e -> {
       openScreen.dispose();
     });
     
-    openScreen.add(openLabel);
-    openScreen.add(selectButton);
-    openScreen.add(closeButton);
-    
-    openScreen.setSize(520, 350);
-    openScreen.setLayout(null);
-    openScreen.setVisible(true);
+    panel.add(openLabel);
+    panel.add(selectButton);
+    panel.add(closeButton);
   }
   
 //  private static void openFileSelector(FileSelector chooseFile)
