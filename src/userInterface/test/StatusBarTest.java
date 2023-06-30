71ad694680ce84d44fe2a20d43f7cbe8d12db7d3
Jeff Baker <git@jbaker.net>
2023-06-30 08:42:53 -0600

    Trying to create JUnit test

---
+++ src/userInterface/test/StatusBarTest.java
@@ -0,0 +1,24 @@
+package userInterface.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import userInterface.StatusBar;
+
+class StatusBarTest
+{
+  @Before
+  public void instantiateStatusBar()
+  {
+    StatusBar status = new StatusBar();
+  }
+  
+  @Test
+  void testUpdate()
+  {
+//    assertEquals(StatusBar.update("Test",1),2);
+  }
+
+}
